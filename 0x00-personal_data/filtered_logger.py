1be6d9b22d77d302358639e61775834747b30d0f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-13 14:23:46 +0200

    filtered_logger.py v0.009

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -51,7 +51,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Returns a database connection. """
     return mysql.connector.connect(user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
                                    password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
