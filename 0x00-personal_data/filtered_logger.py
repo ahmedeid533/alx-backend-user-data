8a8cdba860f707c2ddcc9d6dcfab569b26dde492
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-13 13:45:23 +0200

    filtered_logger.py v0.0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,8 +11,8 @@ import sys
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-	""" Returns the log message obfuscated. """
-	for field in fields:
-		message = re.sub(f'{field}=.*?{separator}',
-						 f'{field}={redaction}{separator}', message)
-	return message
+    """ Returns the log message obfuscated. """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
