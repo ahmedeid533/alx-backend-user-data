8e066d13c9e60b4bd5961251f7bccc1179278923
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-13 13:47:12 +0200

    filtered_logger.py v0.002

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Module for filtering log messages. """
+
 import re
 from typing import List
-
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ Returns the log message obfuscated. """
