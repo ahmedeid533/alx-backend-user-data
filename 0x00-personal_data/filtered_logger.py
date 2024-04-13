6984f8b7e775af9787a0f3008808c8fd0ea85a37
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-13 13:43:46 +0200

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module for filtering log messages. """
+import re
+from typing import List
+import os
+import mysql.connector
+import logging
+import sys
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+	""" Returns the log message obfuscated. """
+	for field in fields:
+		message = re.sub(f'{field}=.*?{separator}',
+						 f'{field}={redaction}{separator}', message)
+	return message
