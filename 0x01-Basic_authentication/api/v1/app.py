57eef48064dcdc5b6e9f32c39138da47a072ee6f
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-17 01:38:23 +0200

    filter

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,9 +13,28 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if os.getenv("AUTH_TYPE") == "basic_auth":
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
+@app.before_request
+def filter_request():
+    """Filter requests
+    """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 @app.errorhandler(404)
@@ -37,20 +56,6 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """ Before request handler
-    """
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth.require_auth(request.path, excluded_paths):
-        if not auth.authorization_header(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
 
 
 if __name__ == "__main__":
