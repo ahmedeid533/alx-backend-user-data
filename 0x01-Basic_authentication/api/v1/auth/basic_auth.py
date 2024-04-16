06a560264c1b54b71a82fba58b8c3397ad3b1499
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-17 01:42:49 +0200

    80 > 79

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ extract_base64_authorization_header
         """
         if authorization_header is None or type(authorization_header) is not str:
